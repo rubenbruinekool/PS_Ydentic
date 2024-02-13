2e6f35fe0106b11965c1d8560aba6d441ff58eee
R. Bruinekool <122553555+rubenbruinekool@users.noreply.github.com>
2024-02-13 13:07:05 +0100

    Update PS_Ydentic.psm1
    Add functions set-YDbaseurl, get-YDbaseurl, remove-YDbaseURL, get-YDCompanyforauditlog,

--- PS_Ydentic/PS_Ydentic.psm1
+++ PS_Ydentic/PS_Ydentic.psm1
@@ -2,25 +2,77 @@
  .Synopsis
   All powershell commandos, for simple manage your Ydentic API commando's.
 #>
+$version = 0.0.6
+
+$script:baseurl = ""
+
+
+##SET
+Function set-YDbaseurl{
+    param(
+        [string]$url
+    )
+    $script:baseUrl = $url
+}
+
+
+##Get
+function get-YDbaseurl{
+    write-host $script:baseurl
+}
 
 Function get-YDtoken
 {
     Param(
-        [string] $baseurl
         [string] $ydentictoken
     )
     
     $header = @{
         "yd-token"=$ydentictoken
     }
-    $uri = "https://idm.controlcat.nl/api/v2/Company.Service/auth"
+    $uri = $script:baseurl + "/api/v2/Company.Service/auth"
     $method = "POST"
     $ContentType = "application/json"
 
     $tokenResponse = Invoke-RestMethod -Method $method -ContentType $ContentType -Uri $uri -Headers $header
+    return $tokenResponse
+}
 
+Function get-YDCompanyforauditlog{
+    Param(
+        [string] $authtoken,
+        [string] $companyname
+    )
+    $headers = @{
+        "Authorization"= "Bearer $authtoken"
+    }
+    $uri = $script:baseUrl + "/api/v2/AuditLogging.Services.AuditLogInfo/get-log-tenants"
+    $method = "Post"
+
+    $companyquery = Invoke-WebRequest -Method $method -Uri $uri -Headers $headers -ErrorAction Stop
+    $companys = $companyquery | ConvertFrom-Json
+    If($null -ne $companyname){
+        $company = $companys | Where-Object {$_.name -eq $companyname}
+    }
+    return $company
 }
 
 
+function get-YDmoduleversion {
+    write-host $version
+  }
+
+##Remove
+function remove-YDbaseURL {
+    $script:baseUrl = ""
+}
+
+
+
 ##Export Functions
-Export-ModuleMember -Function get-YDtoken,
+Export-ModuleMember -Function set-YDbaseurl,
+get-YDbaseurl,
+remove-YDbaseURL,
+get-YDCompanyforauditlog,
+get-YDtoken,
+get-YDmoduleversion
