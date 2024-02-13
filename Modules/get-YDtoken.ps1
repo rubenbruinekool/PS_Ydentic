7f482fc54f4c4ec5b706b149c2bc208cab4b17f4
R. Bruinekool <122553555+rubenbruinekool@users.noreply.github.com>
2024-02-13 11:15:46 +0100

    Add first module

--- Modules/get-YDtoken.ps1
+++ Modules/get-YDtoken.ps1
@@ -1,18 +1,20 @@
 Function get-YDtoken
 {
     Param(
-        [string] $baseurl
+        [string] $baseurl,
         [string] $ydentictoken
     )
     
     $header = @{
         "yd-token"=$ydentictoken
     }
-    $uri = "https://idm.controlcat.nl/api/v2/Company.Service/auth"
+    $uri = $baseurl+"/api/v2/Company.Service/auth"
     $method = "POST"
     $ContentType = "application/json"
 
     $tokenResponse = Invoke-RestMethod -Method $method -ContentType $ContentType -Uri $uri -Headers $header
-
+    return $tokenResponse
 }
 
+$token = get-ydtoken -baseurl https://idm.controlcat.nl -ydentictoken F20X7PPZTPGOCHSYGCI9
+$token
